b1d60ce10b2f0e7a21ec78d45f7ea3c365370bc1
wujianta <1025683357@qq.com>
2019-06-12 19:35:09 +0800

    adds

--- src/main/java/org/newit/microservice/ebusiness/controller/OrderController.java
+++ src/main/java/org/newit/microservice/ebusiness/controller/OrderController.java
@@ -1,52 +1,43 @@
 package org.newit.microservice.ebusiness.controller;
 
-import java.util.List;
-
 import org.newit.microservice.ebusiness.model.Order;
 import org.newit.microservice.ebusiness.service.OrderService;
-import org.newit.microservice.ebusiness.view.OrderView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import com.alibaba.fastjson.JSONObject;
-import com.google.common.collect.Lists;
+import java.util.List;
 
 @Controller
 public class OrderController{
 
     @Autowired
     private OrderService orderService;
-    
 
-
-
-    @RequestMapping(value = "/order/detail/{orderId}",method = RequestMethod.GET)
+    @RequestMapping(value = "/add/order")
     @ResponseBody
-    public Order orderDetail(@PathVariable Long orderId) {
-        Order order = orderService.getOrderById(orderId);
-        return order;
-    }
-
-    @RequestMapping(value = "/order/buyerList/{buyerId}",method = RequestMethod.GET)
-    @ResponseBody
-    public List<Order> buyerOrderList(@PathVariable Long buyerId) {
-
-        return orderService.getOrderListByBuyerId(buyerId);
-    }
-
-
-    @RequestMapping(value = "/order/sellerList/{sellerId}",method = RequestMethod.GET)
-    @ResponseBody
-    public List<Order> sellerOrderList(@PathVariable Long sellerId) {
-        return orderService.getOrderListBySellerId(sellerId);
-    }
-
-    @RequestMapping(value = "/order/create",method = RequestMethod.POST)
-    @ResponseBody
-    public String createOrder(@RequestBody Order order) {
+    public String addOrder(@RequestBody Order order) {
         orderService.createOrder(order);
         return "success";
+    }
+
+    @RequestMapping(value = "/order/{orderId}")
+    @ResponseBody
+    public Order orderDetail(@PathVariable("orderId") long orderId) {
+        return orderService.getOrderById(orderId);
+    }
+
+    @RequestMapping("/order/buyerList")
+    @ResponseBody
+    public List<Order> buyerOrderList(@RequestParam("buyerId")long buyerId) {
+        List<Order> orderList =  orderService.getOrderListByBuyerId(buyerId);
+        return orderList;
+    }
+
+    @RequestMapping("/order/sellerList")
+    @ResponseBody
+    public List<Order> sellerOrderList(@RequestParam("sellerId")long sellerId) {
+        List<Order> orderList =  orderService.getOrderListBySellerId(sellerId);
+        return orderList;
     }
 }
--- src/main/java/org/newit/microservice/ebusiness/service/OrderService.java
+++ src/main/java/org/newit/microservice/ebusiness/service/OrderService.java
@@ -1,13 +1,11 @@
 package org.newit.microservice.ebusiness.service;
 
-import java.util.Calendar;
-import java.util.List;
-
 import org.newit.microservice.ebusiness.model.Order;
 import org.newit.microservice.ebusiness.repository.OrderRepository;
-import org.newit.microservice.ebusiness.view.OrderView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -15,19 +13,11 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-
-
     public Order getOrderById(Long orderId) {
         return orderRepository.getOrderById(orderId);
     }
 
-    public OrderView getOrderView(Order order){
-        OrderView orderView = new OrderView();
-
-        return orderView;
-    }
     public void createOrder(Order order) {
-
         orderRepository.insert(order);
     }
 
